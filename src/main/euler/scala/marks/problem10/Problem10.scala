3db69955462acca5869f17e0d9ed81735127fd25
Mark Slater <mark.slater@mail.com>
2011-07-31 20:23:17 +0100

    Stream solution to problem 10 that's faster, but runs out of memory.

--- src/main/euler/scala/marks/problem10/Problem10.scala
+++ src/main/euler/scala/marks/problem10/Problem10.scala
@@ -1,23 +1,28 @@
 package euler.scala.marks.problem10
 
-import collection.immutable.{TreeSet, SortedSet}
+import collection.Iterable
 
 object Problem10 {
 
   def main(args: Array[String]) {
-    val primes: Stream[Int] = sieve(new TreeSet[Int]().++(2 to 20000))
-    for (prime <- primes) {
-      println(prime)
-    }
-    //    println(sieve(new TreeSet[Int]().++(2 to 20000)).reduceLeft(_ + _))
-    //    println(sieve(new TreeSet[Int]().++(2 to 2000000)).reduceLeft(_ + _))
+    //    val primes: Stream[Int] = sieve(200000)
+    //    for (prime <- primes) {
+    //      println(prime)
+    //    }
+    println(sieve(200000).reduceLeft(_ + _))
   }
 
-  def sieve(candidatePrimes: SortedSet[Int]): Stream[Int] = {
-    if (candidatePrimes.isEmpty) {
-      Stream.empty;
-    } else {
-      Stream.cons(candidatePrimes.head, sieve(candidatePrimes.filter(_ % candidatePrimes.head != 0)));
+  def sieve(maxPrime: Int): Stream[Int] = {
+    def sieve(candidatePrimes: Iterable[Int]): Stream[Int] = {
+      if (candidatePrimes.isEmpty) {
+        Stream.empty;
+      } else {
+        val nextPrime: Int = candidatePrimes.head
+        val remainingCandidates: Iterable[Int] = candidatePrimes.filter(_ % nextPrime != 0)
+        Stream.cons(nextPrime, sieve(remainingCandidates));
+      }
     }
+    Stream.cons(2, sieve(3 to maxPrime by 2));
   }
+
 }
