57ba203e13315a35c052cccfbf1761a632cd69e2
Mark Slater <mark.slater@mail.com>
2011-08-13 09:50:09 +0100

    Handle integer overflow, and accumulate sum, rather than a chunky list.

--- src/main/euler/scala/marks/problem10/Problem10.scala
+++ src/main/euler/scala/marks/problem10/Problem10.scala
@@ -5,20 +5,20 @@ import collection.Iterable
 object Problem10 {
 
   def main(args: Array[String]) {
-    println(sieve(2000000).reduceLeft(_ + _))
+    println(sieveAndSum(2000000))
   }
 
-  def sieve(maxPrime: Int): Iterable[Int] = {
-    def sieve(candidatePrimes: Iterable[Int], primes: List[Int]): List[Int] = {
+  def sieveAndSum(maxPrime: Int): Long = {
+    def sieve(candidatePrimes: Iterable[Int], sumOfPrimes: Long): Long = {
       if (candidatePrimes.isEmpty)
-        primes
+        sumOfPrimes
       else {
         val nextPrime: Int = candidatePrimes.head
         val remainingCandidates: Iterable[Int] = candidatePrimes.filter(_ % nextPrime != 0)
-        sieve(remainingCandidates, nextPrime :: primes)
+        sieve(remainingCandidates, sumOfPrimes + nextPrime)
       }
     }
-    2 :: sieve(3 to maxPrime by 2, Nil)
+    sieve(3 to maxPrime by 2, 2L)
   }
 
 }
