86db9752bb84d71246c15fb99331a26549754669
Mark Slater <mark.slater@mail.com>
2011-08-13 09:55:50 +0100

    Tidy

--- src/main/euler/scala/marks/problem10/Problem10.scala
+++ src/main/euler/scala/marks/problem10/Problem10.scala
@@ -9,16 +9,16 @@ object Problem10 {
   }
 
   def sieveAndSum(maxPrime: Int): Long = {
-    def sieve(candidatePrimes: Iterable[Int], sumOfPrimes: Long): Long = {
+    def sieveAndSum(candidatePrimes: Iterable[Int], sumOfPrimes: Long): Long = {
       if (candidatePrimes.isEmpty)
         sumOfPrimes
       else {
         val nextPrime: Int = candidatePrimes.head
         val remainingCandidates: Iterable[Int] = candidatePrimes.filter(_ % nextPrime != 0)
-        sieve(remainingCandidates, sumOfPrimes + nextPrime)
+        sieveAndSum(remainingCandidates, sumOfPrimes + nextPrime)
       }
     }
-    sieve(3 to maxPrime by 2, 2L)
+    sieveAndSum(3 to maxPrime by 2, 2L)
   }
 
 }
