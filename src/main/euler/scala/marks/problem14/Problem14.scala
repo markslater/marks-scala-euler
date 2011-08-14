b671868ef8c17294fb63387937ef2895e97e2e83
Mark Slater <mark.slater@mail.com>
2011-08-14 18:20:28 +0100

    Tail-recursive breadth first version of problem 14.

--- src/main/euler/scala/marks/problem14/Problem14.scala
+++ src/main/euler/scala/marks/problem14/Problem14.scala
@@ -4,12 +4,13 @@ import java.lang.Boolean
 
 object Problem14 {
   def main(args: Array[String]) {
-    println(moreTerms((1 to 1000000).toSet).dropWhile(_.size > 1).head)
+    println(moreTerms((1 to 1000000).toSet))
   }
 
-  def moreTerms(candidates: Set[Int], numberOfTerms: Int = 1): Stream[Set[Int]] = {
+  def moreTerms(candidates: Set[Int], numberOfTerms: Int = 1): Int = {
     val newCandidates: Set[Int] = candidates.filter(hasTerm(_, numberOfTerms))
-    Stream.cons(newCandidates, if (newCandidates.isEmpty) Stream.empty else moreTerms(newCandidates, numberOfTerms + 1))
+    if (newCandidates.size == 1) newCandidates.head
+    else moreTerms(newCandidates, numberOfTerms + 1)
   }
 
   def hasTerm(firstTerm: Int, termNumber: Int): Boolean = {
