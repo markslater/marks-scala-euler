880cf5953709f8e3212e9d73a8177a1869bca25c
Mark Slater <mark.slater@mail.com>
2011-08-14 14:50:25 +0100

    Problem 12.

--- src/main/euler/scala/marks/problem12/Problem12.scala
+++ src/main/euler/scala/marks/problem12/Problem12.scala
@@ -2,7 +2,7 @@ package euler.scala.marks.problem12
 
 object Problem12 {
   def main(args: Array[String]) {
-    for (i <- triangularNumbers()) println(i)
+    println(triangularNumbers().find(factors(_).length > 500));
   }
 
   def triangularNumbers(term: Int = 1): Stream[Long] = {
@@ -12,4 +12,11 @@ object Problem12 {
   def triangularNumber(n: Long): Long = {
     ((1 + n) * n) / 2
   }
+
+  def factors(number: Long, currentCandidate: Long = 1): List[Long] = {
+    if ((currentCandidate * currentCandidate) > number) List.empty[Long];
+    else if (number % currentCandidate == 0) number / currentCandidate :: ((currentCandidate) :: factors(number, currentCandidate + 1));
+    else factors(number, currentCandidate + 1);
+  }
+
 }
