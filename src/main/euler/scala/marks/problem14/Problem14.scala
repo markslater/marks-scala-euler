13f401e9e5b8721a4cf8861039bf170cc7f5c5ea
Mark Slater <mark.slater@mail.com>
2011-08-14 16:03:03 +0100

    Caching version of problem 14 (slow still)

--- src/main/euler/scala/marks/problem14/Problem14.scala
+++ src/main/euler/scala/marks/problem14/Problem14.scala
@@ -1,12 +1,22 @@
 package euler.scala.marks.problem14
 
+import collection.mutable.Map
+
 object Problem14 {
   def main(args: Array[String]) {
-    println((1 to 1000000).maxBy(theSequence(_)));
+    val map: Map[Int, Int] = Map(1 -> 1)
+    println((1 to 1000000).maxBy(theSequence(_, map)));
   }
 
-  def theSequence(startingPoint: Int, lengthSoFar: Int = 0): Int = {
-    if (startingPoint == 1) 1 + lengthSoFar
-    else theSequence(if ((startingPoint % 2 == 0)) startingPoint / 2 else (startingPoint * 3) + 1, lengthSoFar + 1)
+  def cachingSequence(startingPoint: Int, previousSolutions: Map[Int, Int], lengthSoFar: Int = 0): Int = {
+    val result: Int = theSequence(startingPoint, previousSolutions)
+    previousSolutions.put(startingPoint, result)
+    result;
+  }
+
+  def theSequence(startingPoint: Int, previousSolutions: Map[Int, Int], lengthSoFar: Int = 0): Int = {
+    if (previousSolutions.contains(startingPoint)) lengthSoFar + previousSolutions.get(startingPoint).get
+    else if (startingPoint == 1) 1 + lengthSoFar
+    else theSequence(if ((startingPoint % 2 == 0)) startingPoint / 2 else (startingPoint * 3) + 1, previousSolutions, lengthSoFar + 1)
   }
 }
