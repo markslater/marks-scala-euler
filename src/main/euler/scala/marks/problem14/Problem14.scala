71eed317983346d408fa1c212dce47809ea19725
Mark Slater <mark.slater@mail.com>
2011-08-14 15:22:05 +0100

    Tail-recursive version of problem 14.

--- src/main/euler/scala/marks/problem14/Problem14.scala
+++ src/main/euler/scala/marks/problem14/Problem14.scala
@@ -2,15 +2,11 @@ package euler.scala.marks.problem14
 
 object Problem14 {
   def main(args: Array[String]) {
-    println(theSequence(13))
+    println((1 to 1000000).maxBy(theSequence(_)));
   }
 
-  def theSequence(startingPoint: Int): List[Int] = {
-    if (startingPoint == 1) List(1)
-    else if (startingPoint % 2 == 0) {
-      startingPoint :: theSequence(startingPoint / 2)
-    } else {
-      startingPoint :: theSequence((startingPoint * 3) + 1)
-    }
+  def theSequence(startingPoint: Int, lengthSoFar: Int = 0): Int = {
+    if (startingPoint == 1) 1 + lengthSoFar
+    else theSequence(if ((startingPoint % 2 == 0)) startingPoint / 2 else (startingPoint * 3) + 1, lengthSoFar + 1)
   }
 }
